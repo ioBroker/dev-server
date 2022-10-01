ab66f499e8fc3a1a4cbda713dedc7f084258acd4
Ingo Fischer <github@fischer-ka.de>
2022-10-01 14:16:45 +0200

    - (Apollon77/kleinOr) Set several other system settings on "setup" call like the location, currency, language and temperature unit that they are not empty, fixes #237

--- src/index.ts
+++ src/index.ts
@@ -1331,6 +1331,14 @@ class DevServer {
       systemConfig.common.licenseConfirmed = true; // Disable license confirmation
       systemConfig.common.defaultLogLevel = 'debug'; // Set default log level for adapters to debug
       systemConfig.common.activeRepo = 'beta'; // Set adapter repository to beta
+      // Set other details to dummy values that they are not empty like in a normal installation
+      systemConfig.common.city = 'Berlin';
+      systemConfig.common.country = 'Germany';
+      systemConfig.common.longitude = 13.28;
+      systemConfig.common.latitude = 52.5;
+      systemConfig.common.language = 'en';
+      systemConfig.common.tempUnit = '°C';
+      systemConfig.common.currency = '€';
       return systemConfig;
     });
   }
